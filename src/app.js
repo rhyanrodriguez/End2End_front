46fea6ece92de08ae2d524632003d1142b56649d
rhyanrodriguez <rhyanleal9@gmail.com>
2024-04-17 15:05:52 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './index';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
+reportWebVitals();
