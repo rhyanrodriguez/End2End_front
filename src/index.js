709f242251d30c53f42937f89a9400aec0d891f6
rhyanrodriguez <rhyanleal9@gmail.com>
2024-04-17 15:06:44 -0300

    Increases

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./home";
-import Login from "./login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
 import './App.css';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-function App(){
+function App() {
   const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState("")
 
-  return(
+  useEffect(() => {
+   
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if (!user || !user.token) {
+      setLoggedIn(false)
+      return
+    }
+
+    fetch("http://localhost:3080/verify", {
+            method: "POST",
+            headers: {
+                'jwt-token': user.token
+              }
+        })
+        .then(r => r.json())
+        .then(r => {
+            setLoggedIn('success' === r.message)
+            setEmail(user.email || "")
+        })
+  }, [])
+
+  return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path= "/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}/>
-        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail}/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
