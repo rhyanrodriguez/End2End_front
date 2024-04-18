77e0d4e674e165d2e658fa52b15e1193fda12472
rhyanrodriguez <rhyanleal9@gmail.com>
2024-04-18 08:48:14 -0300

    Freakin out my mind

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Login from './login';
 import App from './index';
 import reportWebVitals from './reportWebVitals';
+import Home from './home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<Home/>);
 
 reportWebVitals();
