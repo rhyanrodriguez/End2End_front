2742cfbf9a52552d7f9f73767db8bb52d7d3cc56
rhyanrodriguez <rhyanleal9@gmail.com>
2024-04-18 10:35:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './login';
 import App from './index';
 import reportWebVitals from './reportWebVitals';
-import Home from './home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home/>);
+root.render(<App/>);
 
 reportWebVitals();
